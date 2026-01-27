42f2ecd0620e4f70f1bd51579bb0f5eeb4d5cd01
Dev Kraken <soman@devkraken.com>
2026-01-27 16:07:47 +0400

    feat(navigation-menu): enhance navigation menu with new Tools section and ListItem component

--- components/layouts/site-header/nav.tsx
+++ components/layouts/site-header/nav.tsx
@@ -1,10 +1,14 @@
+import { type Icon, IconBrandGithub } from '@tabler/icons-react';
+import { type Route } from 'next';
 import Link from 'next/link';
 
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu';
 
 const SiteNav = () => {
@@ -26,9 +30,52 @@ const SiteNav = () => {
             </Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="font-oxanium bg-transparent text-base font-medium text-neutral-400 transition-colors hover:bg-transparent hover:text-neutral-100">
+              Tools
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="w-96">
+                <ListItem href="/tools/github-banner-generator" title="GitHub Banner Generator" icon={IconBrandGithub}>
+                  Create custom SVG banners for GitHub READMEs. Free, themed, and export-ready.
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
       </NavigationMenuList>
     </NavigationMenu>
   );
 };
 
 export default SiteNav;
+
+interface ListItemProps extends React.ComponentPropsWithoutRef<'li'> {
+  title: string;
+  children: React.ReactNode;
+  href: string;
+  icon: Icon;
+}
+const ListItem = ({ title, children, href, icon: IconComp, ...props }: ListItemProps) => {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <Link
+          href={href as Route<string>}
+          title={title}
+          aria-label={title}
+          className="transition-colors hover:text-neutral-100"
+        >
+          <div className="flex items-start justify-start gap-2 text-sm">
+            {IconComp && <IconComp className="size-12 text-neutral-400" strokeWidth={1.5} />}
+            <div className="font-oxanium flex flex-col gap-1">
+              <div className="text-base leading-none font-medium">{title}</div>
+              <div className="text-muted-foreground line-clamp-2 text-sm">{children}</div>
+            </div>
+          </div>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+};
